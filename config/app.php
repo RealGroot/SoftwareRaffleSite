d627ec6cb6fe927e4760c5f762fe56d59a68feeb
Jan Krüger <sirius708@users.noreply.github.com>
2019-08-30 11:00:55 +0200

    :heavy_plus_sign: Added debugbar dev dependency

--- config/app.php
+++ config/app.php
@@ -175,7 +175,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+
 		Zizaco\Entrust\EntrustServiceProvider::class,
+		Barryvdh\Debugbar\ServiceProvider::class,
 
     ],
 
@@ -229,6 +231,7 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
 
 		'Entrust'   => Zizaco\Entrust\EntrustFacade::class,
+		'Debugbar' => Barryvdh\Debugbar\Facade::class,
 
     ],
 
