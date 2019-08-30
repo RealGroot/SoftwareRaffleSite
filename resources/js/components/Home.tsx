62b874c43a1ee4b991120d39422dcda8a0a4fe6e
Jan Krüger <sirius708@users.noreply.github.com>
2019-08-30 09:46:54 +0200

    :construction: Revert reactjs to php blade with vuejs

--- resources/js/components/Home.tsx
+++
@@ -1,9 +0,0 @@
-import React, {Component, ReactElement} from "react";
-
-class Home extends Component {
-	public render(): ReactElement {
-		return <></>;
-	}
-}
-
-export default Home;
--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/{path?}', 'app')->where(['path' => '.*']);//->middleware('auth');
-
-/*
 Auth::routes(['register' => false]);
 
 Route::middleware('auth')->group(function () {
@@ -34,4 +31,3 @@ Route::middleware('auth')->group(function () {
 
 	Route::resource('keys', 'SoftwareKeyController')->only('index')->name('index', 'keys');
 });
-*/
